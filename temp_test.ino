8ad9dd4b17f5aa0d1f647305d809b4733b340eab
Patwater0 <46329995+Patwater0@users.noreply.github.com>
2020-08-06 09:06:33 -0400

    Add files via upload

---
+++ temp_test.ino
@@ -0,0 +1,42 @@
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+#include <LiquidCrystal.h>
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+DHT dht(7,DHT11);
+
+float temp = dht.readTemperature();
+float humid = dht.readHumidity();
+
+void setup() {
+  // start the dht temp/humidity sensor
+  dht.begin();
+  
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  
+  // Print a message to the LCD.
+  lcd.print("Temp  | Humidity");
+}
+
+void loop() {
+  // set the cursor to column 0, line 1
+  lcd.setCursor(0, 1);
+
+  // checks the temp / humidity and converts celcius to ferenheit
+  temp = dht.readTemperature();
+  humid = dht.readHumidity();
+  temp = (temp*9/5) + 32;
+  
+
+  // print the number of seconds since reset:
+  lcd.print(String(temp) + " | " + String(humid));
+  
+  // delay the loop 2 seconds
+  delay(2000);
+}
